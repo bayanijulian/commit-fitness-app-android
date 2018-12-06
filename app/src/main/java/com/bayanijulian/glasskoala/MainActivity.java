da1aaeb656832d780c4f863e8ec6d28b3a487ebd
Bayani Julian <bjulia2@illinois.edu>
2018-12-06 01:13:46 -0600

    created basic layout CreateGoalActivity with FAB in MainActivity

--- app/src/main/java/com/bayanijulian/glasskoala/MainActivity.java
+++ app/src/main/java/com/bayanijulian/glasskoala/MainActivity.java
@@ -1,13 +1,16 @@
 package com.bayanijulian.glasskoala;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView goalsList;
-
+    private FloatingActionButton createGoalFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         GoalsAdapater goalsAdapater = new GoalsAdapater(mockGoalsData());
         goalsList.setAdapter(goalsAdapater);
+
+        createGoalFab = findViewById(R.id.activity_main_fab_create_goal);
+        createGoalFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newCreateGoalActivity = new Intent(v.getContext(), CreateGoalActivity.class);
+                startActivity(newCreateGoalActivity);
+            }
+        });
     }
 
     private Goal[] mockGoalsData() {
