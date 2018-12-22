553a8d401af8197a2e02a635ec9bfad46872474b
Bayani Julian <bjulia2@illinois.edu>
2018-12-22 03:23:54 -0600

    implemented profile fragment

--- app/src/main/java/com/bayanijulian/glasskoala/ui/ProfileFragment.java
+++ app/src/main/java/com/bayanijulian/glasskoala/ui/ProfileFragment.java
@@ -8,13 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.bayanijulian.glasskoala.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
     private static final String TAG = ProfileFragment.class.getSimpleName();
     private Button logoutBtn;
+
+    private ImageButton profileBtn;
+    private TextView nameTxt;
+
+    private FirebaseUser currentUser;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -26,6 +34,8 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
         logoutBtn = view.findViewById(R.id.fragment_profile_btn_logout);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +46,13 @@ public class ProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        nameTxt = view.findViewById(R.id.fragment_profile_tv_name);
+
+        String name = currentUser.getDisplayName();
+        nameTxt.setText(name);
+
+
         return view;
     }
 
